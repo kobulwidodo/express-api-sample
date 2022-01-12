4237df45a7ee2ed82420c95fad955006ef53f382
kobulwidodo <fadhilhan01@gmail.com>
2022-01-12 22:00:27 +0700

    Add Middleware for some routes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,11 +2,11 @@ const productController = require('../controllers/productController')
 const router = require('express').Router()
 const jwtMiddleware = require('../middleware/jwt')
 
-router.post('/', productController.createProduct)
+router.post('/', jwtMiddleware, productController.createProduct)
 router.get('/', productController.getAllProducts)
 router.get('/published', productController.getPublishedProduct)
 router.get('/:id', productController.getProductsById)
-router.put('/:id', productController.updateProductById)
-router.delete('/:id', productController.deleteProduct)
+router.put('/:id', jwtMiddleware, productController.updateProductById)
+router.delete('/:id', jwtMiddleware, productController.deleteProduct)
 
 module.exports = router
