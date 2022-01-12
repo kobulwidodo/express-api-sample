b1b0563b5c15a2468fc25a3c2676448c6c06d150
kobulwidodo <fadhilhan01@gmail.com>
2022-01-12 13:23:07 +0700

    Add product schema for validation

---
+++ validation/productSchema.js
@@ -0,0 +1,23 @@
+const Joi = require('@hapi/joi')
+
+const createProduct = Joi.object({
+  title: Joi.string()
+    .required(),
+  price: Joi.number()
+    .required(),
+  description: Joi.string()
+    .required(),
+  published: Joi.boolean()
+})
+
+const updateProduct = Joi.object({
+  title: Joi.string(),
+  price: Joi.number(),
+  description: Joi.string(),
+  published: Joi.boolean()
+})
+
+module.exports = {
+  createProduct,
+  updateProduct
+}
