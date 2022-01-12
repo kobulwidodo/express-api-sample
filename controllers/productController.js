5edbeca04417f13e4fcc97dc13908b86cc332099
kobulwidodo <fadhilhan01@gmail.com>
2022-01-12 12:56:55 +0700

    Add Api response and try catch

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,58 +1,83 @@
+const apiResponse = require('../helpers/apiResponse')
 const db = require('../models')
 
 const Product = db.products
 
 // Create Product
 const createProduct = async (req, res) => {
-  let body = {
-    title: req.body.title,
-    price: req.body.price,
-    description: req.body.description,
-    published: req.body.published ? req.body.published : false
-  }
+  try {
+    let body = {
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false
+    }
 
-  const product = await Product.create(body)
-  res.status(200).send(product)
+    const product = await Product.create(body)
+    res.status(200).send(apiResponse('success', 200, 'Produk Berhasil dibuat', product))
+  } catch (err) {
+    res.status(422).send(apiResponse('error', 422, err.message))
+  }
 }
 
 // Get All Products
 const getAllProducts = async (req, res) => {
-  let products = await Product.findAll({})
-  res.status(200).send(products)
+  try {
+    let products = await Product.findAll({})
+    res.status(200).send(apiResponse('success', 200, 'Produk berhasil didapatkan', products))
+  } catch (err) {
+    res.status(400).send(apiResponse('error', 400, err.message))
+  }
 }
 
 // Get Product by ID
 const getProductsById = async (req, res) => {
-  let id = req.params.id
-  let product = await Product.findOne({ where: {id: id} })
-  res.status(200).send(product)
+  try {
+    let id = req.params.id
+    let product = await Product.findOne({ where: {id: id} })
+    res.status(200).send(apiResponse('success', 200, 'Berhasil medapatkan produk', product))
+  } catch (err) {
+    res.status(400).send(apiResponse('error', 400, err.message))
+  }
 }
 
 // Get Published Product
 const getPublishedProduct = async (req, res) => {
-  let product = await Product.findAll({ where: { published: true }})
-  res.status(200).send(product)
+  try {
+    let product = await Product.findAll({ where: { published: true }})
+    res.status(200).send(apiResponse('success', 200, 'Berhasil mendapatkan produk', product))
+  } catch (err) {
+    res.status(400).send(apiResponse('error', 400, err.message))
+  }
 }
 
 // Update Product
 const updateProductById = async (req, res) => {
-  let id = req.params.id
-  let body = {
-    title: req.body.title,
-    price: req.body.price,
-    description: req.body.description,
-    published: req.body.published ? req.body.published : false
+  try {
+    let id = req.params.id
+    let body = {
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false
+    }
+    let product = await Product.update(body, { where: {id: id} })
+    res.status(200).send(apiResponse('success', 200, 'Berhasil merubah produk', product))
+  } catch (err) {
+    res.status(433).send(apiResponse('error', 433, err.message))
   }
-  let product = await Product.update(body, { where: {id: id} })
-  res.status(200).send(product)
 }
 
 // Delete Product
 const deleteProduct = async (req, res) => {
-  let id = req.params.id
-  
-  await Product.destroy({ where:{ id: id } })
-  res.status(200).send({'msg': 'Product was deleted'})
+  try {
+    let id = req.params.id
+    
+    await Product.destroy({ where:{ id: id } })
+    res.status(200).send(apiResponse('success', 200, 'Berhasil menghapus produk'))
+  } catch (err) {
+    res.status(433).send(apiResponse('error', 433, err.message))
+  }
 }
 
 module.exports = {
